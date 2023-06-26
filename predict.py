f6d3e283b55af73827c5797e184184252cd92e2e
Noah Harasz <ndharasz@live.com>
2023-06-26 13:42:51 -0700

    add response reason logging

--- predict.py
+++ predict.py
@@ -53,6 +53,7 @@ def parse_args():
 
     return args
 
+
 def exit_with_help(error):
     py_version = "_".join(sys.version.split('.')[:2])
     docker_image_path = f"ghcr.io/numerai/numerai_predict_py_{py_version}:latest"
@@ -61,15 +62,20 @@ def exit_with_help(error):
     logging.root.handlers[0].flush()
     logging.root.handlers[0].setFormatter(logging.Formatter("%(message)s"))
 
-    logging.info("-"*80)
+    logging.info("-" * 80)
     logging.info("Debug your pickle model locally via docker command:")
-    logging.info(f'\n docker run -i --rm -v "$PWD:$PWD" {docker_image_path} {docker_args}')
+    logging.info(
+        f'\n docker run -i --rm -v "$PWD:$PWD" {docker_image_path} {docker_args}'
+    )
     logging.info("\nTry our other support resources:")
     logging.info(" [Github]  https://github.com/numerai/numerai-predict")
-    logging.info(" [Discord] https://discord.com/channels/894652647515226152/1089652477957246996")
-    logging.info("-"*80)
+    logging.info(
+        " [Discord] https://discord.com/channels/894652647515226152/1089652477957246996"
+    )
+    logging.info("-" * 80)
 
     sys.exit(error)
+
 
 def main(args):
     logging.getLogger().setLevel(logging.DEBUG if args.debug else logging.INFO)
@@ -100,7 +106,9 @@ def main(args):
             logging.exception(e)
         exit_with_help(1)
     except TypeError as e:
-        logging.error(f"Pickle incompatible with Python{'.'.join(sys.version.split('.')[0:2])}")
+        logging.error(
+            f"Pickle incompatible with Python{'.'.join(sys.version.split('.')[0:2])}"
+        )
         logging.exception(e) if args.debug else logging.error(e)
         exit_with_help(1)
     except ModuleNotFoundError as e:
@@ -173,6 +181,7 @@ def main(args):
                 time.sleep(RETRY_DELAY)
                 RETRY_DELAY **= random.uniform(1, RETRY_EXP)
             elif r.status_code not in [200, 204]:
+                logging.error(r.reason)
                 logging.error(r.text)
                 sys.exit(1)
             else:
